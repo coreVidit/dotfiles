83b348af3c9aea325f0134762aaf9b72e9c87a85
coreVidit <core.vidit@proton.me>
2026-04-07 02:12:24 +0530

    Fixed several issues, almost complete setup

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,11 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
-set -gx STARSHIP_CONFIG ~/.cache/wallust/starship.toml
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+
+
 starship init fish | source
 end
+
+fish_add_path /home/vdt/.spicetify
---
+++ hypr/wallpaper.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+WALLPAPER_DIR="$HOME/wallpapers/"
+#I dont know what the fuck I am doing
+menu() {
+    find "${WALLPAPER_DIR}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | awk '{print "img:"$0}'
+}
+main() {
+    choice=$(menu | rofi -c ~/.config/wofi/wallpaper -s ~/.config/wofi/style-wallpaper.css -dmenu -P "Select Wallpaper:" -n)
+    selected_wallpaper=$(echo "$choice" | sed 's/^img://')
+    awww img "$selected_wallpaper" --transition-type grow --transition-fps 60 --transition-duration .3
+    wal -i "$selected_wallpaper" -n --cols16
+    pkill swayosd-server
+    swayosd-server &
+    swaync-client --reload-css
+    cat ~/.cache/wal/colors-kitty.conf > ~/.config/kitty/current-theme.conf
+    pywalfox update
+    color1=$(awk 'match($0, /color2=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh)
+    color2=$(awk 'match($0, /color3=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh)
+    sed -i "s/^gradient_color_1 = .*/gradient_color_1 = '$color1'/" $cava_config
+    sed -i "s/^gradient_color_2 = .*/gradient_color_2 = '$color2'/" $cava_config
+    source ~/.cache/wal/colors.sh && cp -r $wallpaper ~/wallpapers/pywallpaper.jpg 
+}
+main
+
